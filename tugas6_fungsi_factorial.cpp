d6de23e3c170045429d03986fec84828ff975c7d
agus maulana <agusanaluam@users.noreply.github.com>
2017-04-07 07:15:15 +0700

    tugas fungsi factorial

---
+++ tugas6_fungsi_factorial.cpp
@@ -0,0 +1,13 @@
+
+long long factorial(int nilai){
+	long long hasil;
+	hasil = 1;
+	
+     //for Loop Block
+     for (int counter = 1; counter <= nilai; counter++)
+     {
+         hasil = hasil * counter;
+         
+     }
+     return hasil
+}
